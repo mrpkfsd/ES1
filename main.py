59ed1305de33317479eae6238a0e6135305228ee
mrpkfsd <paul.flook@gmail.com>
2017-01-23 22:53:59 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def cv():
+    for i in range(0,5):
+        print(i)
+        j = 0
+        while j < 5:
+            print(j)
+            j = j + 1
+            if j == 2:
+                print('Ha')
